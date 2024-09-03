384de2df964c20b4db0fda679b48859bdff7a5ce
Robin Opletal <me@robinopletal.com>
2024-09-03 12:13:05 +0200

    fix(user): bug around password

--- user.tf
+++ user.tf
@@ -8,5 +8,5 @@ resource "opentelekomcloud_identity_user_v3" "user" {
   name = var.name
 
   access_type = "programmatic"
-  password    = var.password ? random_password.user_password[0] : null
+  password    = var.password ? random_password.user_password[0].result : null
 }
